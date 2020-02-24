05e7b2fe95fa69b45722b331d3382231cd5622ce
Walker-TW <thomas.w.walker@live.co.uk>
2020-02-24 12:32:29 +0000

    Added withdraw method that allows balance to be subtracted by an inputted amount

--- lib/bank.rb
+++ lib/bank.rb
@@ -8,4 +8,8 @@ class ATM
   def deposit(amount)
     @balance += amount
   end
+
+  def withdraw(amount)
+    @balance -= amount
+  end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -8,9 +8,17 @@ describe ATM do
   end
 
   describe '#deposit' do
-    it 'Allows the balance instance varaible to be changed by the amount inputed (100)' do
+    it 'Allows the balance instance varaible to be increased by the amount inputed (100)' do
       subject.deposit(100)
       expect(subject.balance).to eq(100)
     end
   end
+
+  describe '#withdraw' do
+    it 'Allows the balance instance variable to be subtracted by the amount inputed (100)' do
+      subject.withdraw(100)
+      expect(subject.balance).to eq(-100)
+    end
+  end
+  
 end
