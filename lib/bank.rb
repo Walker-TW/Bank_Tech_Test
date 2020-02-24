29617b1b01b5fc2da28b1f9b5199cbcb17b21b46
Walker-TW <thomas.w.walker@live.co.uk>
2020-02-24 12:25:03 +0000

    Returns an instance variable (balance) at 0

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,3 +1,8 @@
+
 class ATM
 
+  attr_reader :balance
+  def initialize
+    @balance = 0
+  end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,4 +1,9 @@
 require 'bank.rb'
 
 describe ATM do
+  describe '#Init' do
+    it 'Returns a balance value at 0 when initalized' do
+      expect(subject.balance).to eq(0)
+    end
+  end
 end
